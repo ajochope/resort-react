e814e7fbf6cee79b64f80a95a9bb8210709132d4
Oscar Calles <ocalles@gmail.com>
2020-04-30 14:22:32 +0200

    new_mods_14:22:32

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,7 +1,14 @@
 import {createClient} from 'contentful';
-import {REACT_APP_CONTENTFUL_SPACE_ID,REACT_APP_CONTENTFUL_ACCESS_TOKEN} from './env';
+//import {REACT_APP_CONTENTFUL_SPACE_ID,REACT_APP_CONTENTFUL_ACCESS_TOKEN} from './env';
 
+export default createClient({
+    space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
+});
+
+/*
 export default createClient({
     space: REACT_APP_CONTENTFUL_SPACE_ID,
     accessToken: REACT_APP_CONTENTFUL_ACCESS_TOKEN
 });
+*/
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {RoomProvider} from './context';
+require('dotenv').config();
 
 ReactDOM.render(
   <RoomProvider>
